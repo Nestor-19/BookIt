9616b7b21f3a90c721d7c1700aa9e3450341ab09
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-22 22:52:42 +0400

    Added some dependencies and created index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = express();
+
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Successfully connected to MongoDB!")
+    } 
+    catch (error) {
+        throw error
+    }
+}
+
+
+app.listen(3000, () => {
+    connect();
+    console.log(`Listening on port 3000`);
+})
