43af12cc05cae04a32e03c742cdeea4e93183723
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-24 10:41:03 +0400

    Implemented login user route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -20,3 +20,28 @@ export const registerUser = async (req, res, next) => {
         next(error);
     }
 }
+
+export const loginUser = async (req, res, next) => {
+
+    try{
+        const currUser = await User.findOne({username: req.body.username});
+
+        if (!currUser){
+            return next("404, User not found");
+        }
+
+        const iscorrectPassword = await bcrypt.compare(req.body.password, currUser.password);
+
+        if (!iscorrectPassword){
+            return next("400, Incorrect Password or username!");
+        }
+
+        const {password, isAdmin, ...otherUserDetails} = currUser._doc;
+
+        res.status(200).json({...otherUserDetails});
+    }
+    catch (error){
+        next(error);
+    }
+}
+
