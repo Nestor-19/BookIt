7202e636c724109ee5f9cb96a426a64b66490997
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-26 16:58:34 +0400

    Created two pages and implemented the respective routes for the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
+import Home from "./pages/Home/Home";
+import HotelList from "./pages/HotelList/HotelList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App = ()  => {
   return (
-    <div>
-      Testing react app
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/hotels" element={<HotelList />}/>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
