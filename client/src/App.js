fd9f974de84d04e841c84ffc09bdba23dfbc31f3
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-26 17:35:45 +0400

    Developed and rendered Navbar component in home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import Home from "./pages/Home/Home";
 import HotelList from "./pages/HotelList/HotelList";
+import Hotel from "./pages/Hotel/Hotel";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css'
+
 
 const App = ()  => {
   return (
@@ -8,6 +11,7 @@ const App = ()  => {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/hotels" element={<HotelList />}/>
+        <Route path="/hotels/:id" element={<Hotel />}/>
       </Routes>
     </BrowserRouter>
 
