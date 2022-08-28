ee59dd72deeaebb3690d62a41925ba4cc6c03e48
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-28 21:11:52 +0400

    Conditionally rendered title and search bar depending on the page

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,7 +8,7 @@ import {IoBedSharp, IoPerson} from "react-icons/io5";
 import { DateRange} from 'react-date-range';
 import { format } from 'date-fns';
 
-const Header = () => {
+const Header = (props) => {
 
   const [openCalendar, setOpenCalendar] = useState(false);
 
@@ -34,7 +34,7 @@ const Header = () => {
 
   return (
     <div className='header'>
-      <div className='headerContainer'>
+      <div className={props.type === "list" ? "headerContainer listMode" : "headerContainer"}>
         <div className='headerList'>
               <div className='headerListItem active'>
                   <IoBedSharp />
@@ -58,69 +58,72 @@ const Header = () => {
               </div>
         </div>
 
-        <h1 className='title'>Book your next stay</h1>
-        <p className='description'>Search deals on hotels, homes, and much more...</p>
+        {props.type !== "list" &&
+          <> 
+          <h1 className='title'>Book your next stay</h1>
+          <p className='description'>Search deals on hotels, homes, and much more...</p>
 
-        <div className='search'>
-            <div className='searchItem'>
-              <IoBedSharp className='icons'/>
-              <input type="text" placeholder='Where are you going?' className='searchInput'/>
-            </div>
+          <div className='search'>
+              <div className='searchItem'>
+                <IoBedSharp className='icons'/>
+                <input type="text" placeholder='Where are you going?' className='searchInput'/>
+              </div>
 
-            <div className='searchItem'>
-              <FaCalendarAlt className='icons'/>
-              <span className='searchText' onClick={() => setOpenCalendar(!openCalendar)}>
-                {`${format(currentDate[0].startDate, "MM/dd/yyyy")} to ${format(currentDate[0].endDate, "MM/dd/yyyy")} `}
-              </span>
-              {openCalendar && <DateRange 
-              editableDateInputs = {true} 
-              onChange = {item => setCurrentDate([item.selection])}
-              moveRangeOnFirstSelection = {false}
-              ranges = {currentDate}
-              className="dateRange"
-              />}
-            </div>
+              <div className='searchItem'>
+                <FaCalendarAlt className='icons'/>
+                <span className='searchText' onClick={() => setOpenCalendar(!openCalendar)}>
+                  {`${format(currentDate[0].startDate, "MM/dd/yyyy")} to ${format(currentDate[0].endDate, "MM/dd/yyyy")} `}
+                </span>
+                {openCalendar && <DateRange 
+                editableDateInputs = {true} 
+                onChange = {item => setCurrentDate([item.selection])}
+                moveRangeOnFirstSelection = {false}
+                ranges = {currentDate}
+                className="dateRange"
+                />}
+              </div>
 
-            <div className='searchItem'>
-              <IoPerson className='icons'/>
-              <span className='searchText' onClick={() => setOpenOptions(!openOptions)}>
-                {`${roomOptions.adults} adult · ${roomOptions.children} children · ${roomOptions.rooms} room`} 
-              </span>
-              {openOptions && <div className='roomOptions'>
-                <div className='optionItem'>
-                  <span className='optionText'>Adult</span>
-                  <div className='optionCounter'>
-                    <button className='counterButton' onClick={() => handleOptionClick("adults", "i")}>+</button>
-                    <span className='counterNumber'>{roomOptions.adults}</span>
-                    <button disabled={roomOptions.adults <= 1} className='counterButton' onClick={() => handleOptionClick("adults", "d")}>-</button>
+              <div className='searchItem'>
+                <IoPerson className='icons'/>
+                <span className='searchText' onClick={() => setOpenOptions(!openOptions)}>
+                  {`${roomOptions.adults} adult · ${roomOptions.children} children · ${roomOptions.rooms} room`} 
+                </span>
+                {openOptions && <div className='roomOptions'>
+                  <div className='optionItem'>
+                    <span className='optionText'>Adult</span>
+                    <div className='optionCounter'>
+                      <button className='counterButton' onClick={() => handleOptionClick("adults", "i")}>+</button>
+                      <span className='counterNumber'>{roomOptions.adults}</span>
+                      <button disabled={roomOptions.adults <= 1} className='counterButton' onClick={() => handleOptionClick("adults", "d")}>-</button>
+                    </div>
                   </div>
-                </div>
 
-                <div className='optionItem'>
-                  <span className='optionText'>Children</span>
-                  <div className='optionCounter'>
-                    <button className='counterButton' onClick={() => handleOptionClick("children", "i")}>+</button>
-                    <span className='counterNumber'>{roomOptions.children}</span>
-                    <button disabled={roomOptions.children <= 0} className='counterButton' onClick={() => handleOptionClick("children", "d")}>-</button>
+                  <div className='optionItem'>
+                    <span className='optionText'>Children</span>
+                    <div className='optionCounter'>
+                      <button className='counterButton' onClick={() => handleOptionClick("children", "i")}>+</button>
+                      <span className='counterNumber'>{roomOptions.children}</span>
+                      <button disabled={roomOptions.children <= 0} className='counterButton' onClick={() => handleOptionClick("children", "d")}>-</button>
+                    </div>
                   </div>
-                </div>
 
-                <div className='optionItem'>
-                  <span className='optionText'>Room</span>
-                  <div className='optionCounter'>
-                    <button className='counterButton' onClick={() => handleOptionClick("rooms", "i")}>+</button>
-                    <span className='counterNumber'>{roomOptions.rooms}</span>
-                    <button disabled={roomOptions.rooms <= 1} className='counterButton' onClick={() => handleOptionClick("rooms", "d")}>-</button>
+                  <div className='optionItem'>
+                    <span className='optionText'>Room</span>
+                    <div className='optionCounter'>
+                      <button className='counterButton' onClick={() => handleOptionClick("rooms", "i")}>+</button>
+                      <span className='counterNumber'>{roomOptions.rooms}</span>
+                      <button disabled={roomOptions.rooms <= 1} className='counterButton' onClick={() => handleOptionClick("rooms", "d")}>-</button>
+                    </div>
                   </div>
-                </div>
-              </div>}
+                </div>}
 
-            </div>
+              </div>
 
-            <div className='searchItem'>
-              <button className='searchButton'>Search</button>
-            </div>
-        </div>
+              <div className='searchItem'>
+                <button className='searchButton'>Search</button>
+              </div>
+          </div>
+        </>}
       </div>
     </div>
   )
--- client/src/pages/HotelList/HotelList.js
+++ client/src/pages/HotelList/HotelList.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Header from '../../components/Header/Header'
 
 const HotelList = () => {
   return (
-    <div>HotelList</div>
+    <div>
+      <Navbar />
+      <Header type="list"/>
+    </div>
   )
 }
 
