dc66044ba50c327087ab2d9367dc0e816e250bd2
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-28 22:21:50 +0400

    Created Properties component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar';
 import Header from '../../components/Header/Header';
 import Cities from '../../components/Cities/Cities';
+import Properties from '../../components/Properties/Properties';
 
 const Home = () => {
     return (
@@ -11,6 +12,8 @@ const Home = () => {
             <Header />
             <div className='homeContainer'>
                 <Cities />
+                <h1 className='homeTitle'>Browse by property type</h1>
+                <Properties />
             </div>
         </div>
     )
