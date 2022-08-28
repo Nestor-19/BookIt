97347d6d797b73ff8193c9e55eef324adc57c810
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2022-08-28 12:09:23 +0400

    Set up search bar in Header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Header.css';
-import {FaPlane, FaBed, FaCarAlt} from "react-icons/fa";
+import {FaPlane, FaCarAlt, FaCalendarAlt} from "react-icons/fa";
 import {MdAttractions, MdLocalTaxi} from "react-icons/md";
+import {IoBedSharp, IoPerson} from "react-icons/io5";
 
 const Header = () => {
   return (
@@ -9,7 +10,7 @@ const Header = () => {
       <div className='headerContainer'>
         <div className='headerList'>
               <div className='headerListItem active'>
-                  <FaBed />
+                  <IoBedSharp />
                   <span>Stays</span>
               </div>
               <div className='headerListItem'>
@@ -33,6 +34,23 @@ const Header = () => {
         <h1 className='title'>Book your next stay</h1>
         <p className='description'>Search deals on hotels, homes, and much more...</p>
 
+        <div className='search'>
+            <div className='searchItem'>
+              <IoBedSharp className='icons'/>
+              <input type="text" placeholder='Where are you going?' className='searchInput'/>
+            </div>
+            <div className='searchItem'>
+              <FaCalendarAlt className='icons'/>
+              <span className='searchText'> Check-in - Check-out</span>
+            </div>
+            <div className='searchItem'>
+              <IoPerson className='icons'/>
+              <span className='searchText'> 2 adults 0 children 1 room</span>
+            </div>
+            <div className='searchItem'>
+              <button className='searchButton'>Search</button>
+            </div>
+        </div>
       </div>
     </div>
   )
